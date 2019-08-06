f34c486cd16ee1644681c718346d07593284bf5d
Christian Collosi <christian.collosi@gmail.com>
2019-08-05 17:04:42 -0700

    Add background image to splash page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,14 +6,32 @@ import Hidden from '@material-ui/core/Hidden';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import AuthDialog from '../dialogs/AuthDialog';
+import Fitness from '../images/fitness.jpg';
 
 const useStyles = makeStyles(theme => ({
   container: {
+    backgroundImage: `url(${Fitness})`,
+    backgroundSize: '100%',
+    backgroundRepeat: 'no-repeat',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     flexGrow: '1',
+  },
+  containerContent: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    background: 'rgba(204, 204, 204, 0.65)',
+    padding: '10px',
+  },
+  conatianerText: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   button: {
     margin: theme.spacing(1),
@@ -36,24 +54,29 @@ const Welcome = () => {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm" className={classes.container}>
-        <Typography variant="h4" gutterBottom>
-          Welcome to XerTimer
-        </Typography>
-        <Typography variant="body1">
-          XerTimer is a custom interval timer that will assist you in making timers with different
-          lengths for each set. It also allows to make manual sets in the cases where you are doing
-          set number of reps instead of a timed set.
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          className={classes.button}
-          onClick={() => openAuthDialog()}
-        >
-          <Typography variant="button">Get Started</Typography>
-        </Button>
+      <div className={classes.container}>
+        <div className={classes.containerContent}>
+          <Container maxWidth="sm" className={classes.conatianerText}>
+            <Typography variant="h4" gutterBottom>
+              Welcome to XerTimer
+            </Typography>
+            <Typography variant="body1">
+              XerTimer is a custom interval timer that will assist you in making timers with
+              different lengths for each set. It also allows to make manual sets in the cases where
+              you are doing set number of reps instead of a timed set.
+            </Typography>
+
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              className={classes.button}
+              onClick={() => openAuthDialog()}
+            >
+              <Typography variant="button">Get Started</Typography>
+            </Button>
+          </Container>
+        </div>
         {isDialogOpen && (
           <React.Fragment>
             <Hidden only="xs">
@@ -64,7 +87,7 @@ const Welcome = () => {
             </Hidden>
           </React.Fragment>
         )}
-      </Container>
+      </div>
     </React.Fragment>
   );
 };
