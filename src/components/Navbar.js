0e531938354b298617f314b06874db3ee10cb329
christian <christian.collosi@gmail.com>
2019-09-25 19:26:47 -0700

    Remove signout data and state from Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import firebase from './Firebase';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,24 +17,8 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const NavBar = props => {
+const NavBar = () => {
   const classes = useStyles();
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    const getUser = async () => {
-      const user = await firebase.getCurrentUser();
-      setCurrentUser(user);
-    };
-
-    getUser();
-  }, []);
-
-  const signOutUser = async () => {
-    await firebase.logout();
-    debugger;
-    props.history.replace('/');
-  };
 
   return (
     <div className={classes.root}>
@@ -45,11 +27,6 @@ const NavBar = props => {
           <Typography variant='h6' className={classes.title}>
             XerTimer
           </Typography>
-          {currentUser && (
-            <Button color='inherit' onClick={signOutUser}>
-              Sign Out
-            </Button>
-          )}
         </Toolbar>
       </AppBar>
     </div>
